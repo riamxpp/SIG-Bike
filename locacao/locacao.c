c7311bffa4281e16beaf84a20fc7869c368ae3cb
ariadny <ariadnyfran15@gmail.com>
2024-11-03 14:43:58 -0300

    feat: criando a função menuLocacao no view.c

--- locacao/locacao.c
+++ locacao/locacao.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "locacao.h"
+#include <unistd.h>
+#include "view.h"
 
 void locacao(void){
-    int op_locacao;
+    int op_locacao
+    op_locacao = menuLocacao();
     do{
-        system("clear||cls");
-        printf("\n╔═══════════════════════════════════════════════════════════════════════════════╗\n");
-        printf("║                               Módulo Locação                                  ║\n");
-        printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
-        printf("║                            1. Reservar Bicicleta                              ║\n");
-        printf("║                            2. Reservar Patinete                               ║\n");
-        printf("║                            3. Devolver Bicicleta                              ║\n");
-        printf("║                            4. Devolver Patinete                               ║\n");
-        printf("║                            5. Consultar Locação                               ║\n");
-        printf("║                                                                               ║\n");
-        printf("║                            0. Voltar                                          ║\n");
-        printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
-        printf(" Digite a opção desejada: ");
-        scanf("%d", &op_locacao);
-        getchar();
+        
         switch (op_locacao) {
             case 1:
                 reservarBicicleta();
---
+++ locacao/view.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "view.h"
+
+int menuLocacao(void){
+    int op_locacao;
+    system("clear||cls");
+    printf("\n╔═══════════════════════════════════════════════════════════════════════════════╗\n");
+    printf("║                               Módulo Locação                                  ║\n");
+    printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
+    printf("║                            1. Reservar Bicicleta                              ║\n");
+    printf("║                            2. Reservar Patinete                               ║\n");
+    printf("║                            3. Devolver Bicicleta                              ║\n");
+    printf("║                            4. Devolver Patinete                               ║\n");
+    printf("║                            5. Consultar Locação                               ║\n");
+    printf("║                                                                               ║\n");
+    printf("║                            0. Voltar                                          ║\n");
+    printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
+    printf(" Digite a opção desejada: ");
+    scanf("%d", &op_locacao);
+    getchar();
+    return op_locacao;
+}
