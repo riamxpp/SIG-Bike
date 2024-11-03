651f017063f01c80bb730cfb593ed091bb65c008
ariadny <ariadnyfran15@gmail.com>
2024-11-03 14:58:56 -0300

    refactor: relocando duas funções que estavam declaradas no local errado

--- clientes/view.h
+++ clientes/view.h
@@ -5,7 +5,5 @@ int menuClientes(void);
 void menuCadastrarCliente(void);
 void menuPesquisarCliente(void);
 void menuAtualizarCliente(void);
-void menuDevolverBicicleta(void);
-void menuDevolverPatinete(void);
 
 #endif // VIEW_H
--- locacao/view.h
+++ locacao/view.h
@@ -4,5 +4,7 @@
 int menuLocacao(void);
 void menuReservarBicicleta(void);
 void menuReservarPatinete(void);
+void menuDevolverBicicleta(void);
+void menuDevolverPatinete(void);
 
 #endif // VIEW_H
