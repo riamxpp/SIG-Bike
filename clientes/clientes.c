7f195a4491e568e2e7a52d789b1e8638a4499176
Riam Stefesom <riamstefeson616@gmail.com>
2024-11-20 20:57:01 -0300

    refactor: criando as variáveis  da função 'atualizarCliente', adicionando alocação de memória e a validação para todas.

--- clientes/clientes.c
+++ clientes/clientes.c
@@ -130,19 +130,96 @@ void pesquisarCliente(void){
 }
 
 void atualizarCliente(void){
+    char *cpf;
+    char *nome;
+    char *email;
+    char *fone;
+    char *dtnas;
+
     menuAtualizarCliente();
-    printf("║ Digite o CPF do Cliente:                                                      ║\n");
+
+    cpf = (char*) malloc(15*sizeof(char));
+    
+    do{
+        printf("║ Digite o CPF do Cliente: ");      
+        scanf("%14s", cpf);
+        while (getchar() != '\n');
+        if (!validarCPF(cpf)) {
+            printf("  CPF inválido. \n");
+            printf("\n");
+        }
+    }while (!validarCPF(cpf));
+    free(cpf);
+
     printf("║                                                                               ║\n");
     printf("║                           ↪Digite os Novos Dados↩                             ║\n");
-    printf("║ Nome:                                                                         ║\n");
-    printf("║ CPF:                                                                          ║\n");
-    printf("║ Email:                                                                        ║\n");
-    printf("║ Fone:                                                                         ║\n");
-    printf("║ Data de Nascimento:                                                           ║\n");
+    nome = (char*) malloc(100*sizeof(char));
+
+    do {
+        printf("║ Nome: ");
+        scanf("%99[^\n]", nome);
+        while (getchar() != '\n');
+        if (!validarNome(nome)) {
+            printf("  Entrada inválida. Digite apenas letras e espaços.\n");
+            printf("\n");
+        }
+    } while (!validarNome(nome));
+
+    cpf = (char*) malloc(15*sizeof(char));
+    
+    do{
+        printf("║ CPF (xxx.xxx.xxx-xx ou xxxxxxxxxxx): ");
+        scanf("%14s", cpf);
+        while (getchar() != '\n');
+        if (!validarCPF(cpf)) {
+            printf("  CPF inválido. \n");
+            printf("\n");
+        }
+    }while (!validarCPF(cpf));
+
+    email = (char*) malloc(100*sizeof(char));
+    do{ 
+        printf("║ Email: ");
+        scanf("%99s", email);
+        while (getchar() != '\n');
+        if (!validarEmail(email)) {
+            printf("  Email inválido. \n");
+            printf("\n");
+        }
+    }while (!validarEmail(email));
+
+    fone = (char*) malloc(20*sizeof(char));
+    do{
+        printf("║ Telefone (DD000000000): ");
+        scanf("%19s", fone);
+        while (getchar() != '\n');
+        if (!validarTelefone(fone)) {
+            printf("  Telefone inválido. \n");
+            printf(" \n");
+        }
+    }while (!validarTelefone(fone));
+    
+    dtnas = (char*) malloc(11*sizeof(char));
+    do{
+        printf("║ Data de Nascimento: (xx/xx/xxxx): ");
+        scanf("%19s", dtnas);
+        while (getchar() != '\n');
+        if (!validarDataNascimento(dtnas)) {
+            printf("  Data de Nascimento inválida. \n");
+            printf(" \n");
+        }
+    }while (!validarDataNascimento(dtnas));
+
     printf("║                                                                               ║\n");
     printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
     printf("Tecle <ENTER> para continuar...");
     getchar();
+
+    free(cpf);
+    free(nome);
+    free(email);
+    free(fone);
+    free(dtnas);
 }
 
 void deletarCliente(void){
@@ -164,7 +241,7 @@ void deletarCliente(void){
             printf("\n");
         }
     }while (!validarCPF(cpf));
-    
+
     printf("║                                                                               ║\n");
     printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
     printf("Tecle <ENTER> para continuar...");
