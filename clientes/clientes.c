cedc39844d9f0cc1f01dea26b876060344833fbe
Riam Stefesom <riamstefeson616@gmail.com>
2024-11-24 19:32:29 -0300

    refactor: adicionando struct no arquivo clientes.c e já chamando na função cadastrarCliente.

--- clientes/clientes.c
+++ clientes/clientes.c
@@ -5,6 +5,15 @@
 #include "view.h"
 #include "../validacao/validacao.h"
 
+struct cliente {
+    char *nome;
+    char *cpf;
+    char *email;
+    char *fone;
+    char *dtnas;    
+};
+
+
 void clientes(void){
     int op_cliente;
     do{
@@ -32,77 +41,74 @@ void clientes(void){
 }
 
 void cadastrarCliente(void){
-    char *nome;
-    char *cpf;
-    char *email;
-    char *fone;
-    char *dtnas;
+    struct cliente cliente1;
+
     menuCadastrarCliente();
 
-    nome = (char*) malloc(100*sizeof(char));
+    cliente1.nome = (char*) malloc(100*sizeof(char));
     do {
         printf("║ Nome: ");
-        scanf("%99[^\n]", nome);
+        scanf("%99[^\n]", cliente1.nome);
         while (getchar() != '\n');
-        if (!validarNome(nome)) {
+        if (!validarNome(cliente1.nome)) {
             printf("  Entrada inválida. Digite apenas letras e espaços.\n");
             printf("\n");
         }
-    } while (!validarNome(nome));
+    } while (!validarNome(cliente1.nome));
 
-    cpf = (char*) malloc(15*sizeof(char));
+    cliente1.cpf = (char*) malloc(15*sizeof(char));
     do{
         printf("║ CPF (xxx.xxx.xxx-xx ou xxxxxxxxxxx): ");
-        scanf("%14s", cpf);
+        scanf("%14s", cliente1.cpf);
         while (getchar() != '\n');
-        if (!validarCPF(cpf)) {
+        if (!validarCPF(cliente1.cpf)) {
             printf("  CPF inválido. \n");
             printf("\n");
         }
-    }while (!validarCPF(cpf));
+    }while (!validarCPF(cliente1.cpf));
 
-    email = (char*) malloc(100*sizeof(char));
+    cliente1.email = (char*) malloc(100*sizeof(char));
     do{ 
         printf("║ Email: ");
-        scanf("%99s", email);
+        scanf("%99s", cliente1.email);
         while (getchar() != '\n');
-        if (!validarEmail(email)) {
+        if (!validarEmail(cliente1.email)) {
             printf("  Email inválido. \n");
             printf("\n");
         }
-    }while (!validarEmail(email));
+    }while (!validarEmail(cliente1.email));
 
-    fone = (char*) malloc(20*sizeof(char));
+    cliente1.fone = (char*) malloc(20*sizeof(char));
     do{
         printf("║ Telefone (DD000000000): ");
-        scanf("%19s", fone);
+        scanf("%19s", cliente1.fone);
         while (getchar() != '\n');
-        if (!validarTelefone(fone)) {
+        if (!validarTelefone(cliente1.fone)) {
             printf("  Telefone inválido. \n");
             printf(" \n");
         }
-    }while (!validarTelefone(fone));
+    }while (!validarTelefone(cliente1.fone));
 
-    dtnas = (char*) malloc(11*sizeof(char));
+    cliente1.dtnas = (char*) malloc(11*sizeof(char));
     do{
         printf("║ Data de Nascimento: (xx/xx/xxxx): ");
-        scanf("%19s", dtnas);
+        scanf("%19s", cliente1.dtnas);
         while (getchar() != '\n');
-        if (!validarDataNascimento(dtnas)) {
+        if (!validarDataNascimento(cliente1.dtnas)) {
             printf("  Data de Nascimento inválida. \n");
             printf(" \n");
         }
-    }while (!validarDataNascimento(dtnas));
+    }while (!validarDataNascimento(cliente1.dtnas));
     printf("\n╔═══════════════════════════════════════════════════════════════════════════════╗\n");
     printf("║                        Cliente cadastrado com sucesso!                        ║\n");
     printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
     printf("Tecle <ENTER> para continuar...");
     getchar();
-    free(cpf);
-    free(nome);
-    free(email);
-    free(fone);
-    free(dtnas);
+    free(cliente1.cpf);
+    free(cliente1.nome);
+    free(cliente1.email);
+    free(cliente1.fone);
+    free(cliente1.dtnas);
 }
 
 void pesquisarCliente(void){
